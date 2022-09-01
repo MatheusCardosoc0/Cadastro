b55f153616ed4e1adc73994e17f77c3d28a542bc
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-01 10:48:08 -0300

    teste

---
+++ crod/config/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC678kUMKfxWs7q-hJ8sVxpMoh623nYZos",
+  authDomain: "crod-4c7a1.firebaseapp.com",
+  projectId: "crod-4c7a1",
+  storageBucket: "crod-4c7a1.appspot.com",
+  messagingSenderId: "251224459572",
+  appId: "1:251224459572:web:4b78dcccd574020b3c3d61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
